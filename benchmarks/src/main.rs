865391bface646afb7a3a8222919f0d7fba45d60
GeonHyeok Lee <geonhyeoklee.kr@gmail.com>
2025-02-26 23:32:35 +0900

    feat: add download benchmark

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use std::time::Instant;
+
+use image_proc::commands;
+
+#[tokio::main]
+async fn main() {
+  let path = std::path::PathBuf::from("./downloads");
+
+  let start = Instant::now();
+  commands::download::execute(path).await;
+  let duration = start.elapsed();
+
+  println!("Download execution took: {:?}", duration);
+}
